6727f41b4e475a4b65e128f0a72954c0612c5c2a
Alexangelj <alexangel.j@gmail.com>
2021-01-13 22:40:59 -0800

    Runs prettier through code.

--- contracts/libraries/Math.sol
+++ contracts/libraries/Math.sol
@@ -1,7 +1,5 @@
 pragma solidity >=0.5.16;
 
-// a library for performing various math operations
-
 library Math {
     function min(uint x, uint y) internal pure returns (uint z) {
         z = x < y ? x : y;
